df1e76650647983e3a769a9de0f6f07a2d9a42a6
Luiz Fiorindi <luiz.fiorindi@autlog.com.br>
2025-08-12 17:24:34 -0300

    Add Logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,25 @@ const NavBar = async () => {
   const session = await auth();
 
   return (
-    <div>
-      <Link href="/">Home</Link>
+    <div className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      <Link className="text-white text-lg font-bold" href="/">
+        Home
+      </Link>
       <div>
         {session && session.user ? (
           //logged in
-          <div>
+          <div className="flex gap-4 items-center">
             <p>{session.user.name}</p>
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <button className="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded">
+                Sign-Out
+              </button>
+            </form>
           </div>
         ) : (
           //logged out
@@ -21,7 +33,9 @@ const NavBar = async () => {
               await signIn();
             }}
           >
-            <button type="submit">Sign-In</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
+              Sign-In
+            </button>
           </form>
         )}
       </div>
